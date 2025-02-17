d2bc0eca5aa49d04ef4ca6683bcd8a8c73c2fc00
IzmailovRuslan <93521621+IzmailovRuslan@users.noreply.github.com>
2025-02-17 22:24:39 +0300

    Add files via upload

--- Lab1/run.py
+++ Lab1/run.py
@@ -1,14 +1,16 @@
 import os
 import subprocess
 
+timeout = 120
+
 kafka = subprocess.Popen(['docker-compose', 'up'])
 client = subprocess.Popen(['streamlit', 'run', 'app.py'])
 data_get = subprocess.Popen(['python', 'server/interactions_producer.py'])
 
 try:
-    kafka.wait(120)
-    client.wait(120)
-    data_get.wait(120)
+    kafka.wait(timeout)
+    client.wait(timeout)
+    data_get.wait(timeout)
 except:
     kafka.kill()
     client.kill()
